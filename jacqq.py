e5372ca4f9adedd2bd8e59f0c93981a097bba173
Saman Jirjies <saman.jirjies@gmail.com>
2015-09-07 14:57:22 -0700

    Added setup.py

--- jacqq.py
+++ jacqq.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 import sys
 import os
 import scipy.spatial as spatial
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyJacqQ',
+    version='0.4.1',
+    packages=['', 'tests'],
+    url='https://github.com/sjirjies/pyJacqQ',
+    license='GPLv3',
+    author='Saman Jirjies',
+    author_email='sjirjies@asu.edu',
+    description="Open Soure Jacquez's Q-Statistics for Space-Time Clustering"
+)
